200de772920f7d19016ef4ce116b2d74fe86f7b4
Manu <M.laux@wooobee.com>
2013-03-21 15:17:09 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Wondate::Application.config.session_store :cookie_store, key: '_wondate_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Wondate::Application.config.session_store :active_record_store
